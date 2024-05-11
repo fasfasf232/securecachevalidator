9afb5cbb77a95d8e943740abaceb978edcde4cd5
fasfasf232 <js1kaosanne@hotmail.com>
2024-05-11 17:40:10 +0700

    commit

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const binarySearchIterative = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  return -1;
-};
+const groupBy = (arr, func) =>
+  arr
+    .map(typeof func === "function" ? func : (val) => val[func])
+    .reduce((acc, val, i) => {
+      acc[val] = (acc[val] || []).concat(arr[i]);
+      return acc;
+    }, {});
