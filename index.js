bc8cdd88b1eaf8196bdc106252ad1b750b5e27e7
fasfasf232 <js1kaosanne@hotmail.com>
2024-05-13 02:43:38 +0700

    update project

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const groupBy = (arr, func) =>
-  arr
-    .map(typeof func === "function" ? func : (val) => val[func])
-    .reduce((acc, val, i) => {
-      acc[val] = (acc[val] || []).concat(arr[i]);
-      return acc;
-    }, {});
+function maximalSquare(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
+  let maxSide = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        maxSide = Math.max(maxSide, dp[i][j]);
+      }
+    }
+  }
+  return maxSide * maxSide;
+}
