4756828f27e2766e5467748160753929b26fe58c
fasfasf232 <js1kaosanne@hotmail.com>
2024-05-14 02:31:37 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function maximalSquare(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
-  let maxSide = 0;
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= cols; j++) {
-      if (matrix[i - 1][j - 1] === "1") {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-        maxSide = Math.max(maxSide, dp[i][j]);
-      }
+function longestPalindrome(s) {
+  const map = new Map();
+  let hasOdd = false;
+  let result = 0;
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (const count of map.values()) {
+    if (count % 2 === 0) result += count;
+    else {
+      result += count - 1;
+      hasOdd = true;
     }
   }
-  return maxSide * maxSide;
+  return hasOdd ? result + 1 : result;
 }
